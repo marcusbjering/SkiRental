34a69ead3b50af16a8b0f83ef638dea2d4cc4c7a
Marcus Bjering <marcusbjering@outlook.com>
2018-01-17 22:44:46 +0100

    Add project

---
+++ SkiRental/SkiRental.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,23 @@
+﻿using SkiRentalApi.Web.App_Start;
+using System.Web.Http;
+using Unity;
+
+namespace SkiRentalApi.Web
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var container = new UnityContainer();
+
+            RegisterTypes(container);
+            config.DependencyResolver = new UnityResolver(container);
+            config.MapHttpAttributeRoutes();
+        }
+
+        private static void RegisterTypes(UnityContainer container)
+        {
+            //container.RegisterType<>();
+        }
+    }
+}
---
+++ SkiRental/SkiRental.Api/Controllers/CrossCountrySkisController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SkiRentalApi.Web.Controllers
+{
+    public class CrossCountrySkisController : ApiController
+    {
+        //private readonly ICrossCountrySkisRequestValidator
+
+        public IHttpActionResult Get(decimal length, int age, CrossCountrySkiType type)
+        {
+
+
+            return Ok();
+        }
+
+        public enum CrossCountrySkiType
+        {
+            Classic = 1,
+            Skate = 2
+        }
+    }
+}
